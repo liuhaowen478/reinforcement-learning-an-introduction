70a195d6782a94c0cfba324eefce0371b90b7d50
Haowen Liu <liu.haowen.andy@gmail.com>
2020-08-23 17:35:01 -0700

    figure 5.1 in python wireframe

---
+++ chapter05/backjack_exercise.py
@@ -0,0 +1,153 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from tqdm import tqdm
+
+EPS = [10000, 500000]
+VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+def set_axes_equal(ax):
+    ''' https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
+    Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+
+def deal_card(hand, ace):
+    card = VALUES[np.random.randint(13)]
+
+    if card == 1 and hand < 11:
+        new_hand = hand + 11
+        new_ace = 1
+    else:
+        new_hand = hand + card
+        new_ace = ace
+
+    if new_hand > 21 and new_ace == 1:
+        new_hand -= 10
+        new_ace = 0
+
+    return new_hand, new_ace
+
+
+def figure_5_1():
+    plt.rc("font", size=6)
+    fig = plt.figure()
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    fig_count = 1
+
+    for eps in EPS:
+        values = np.zeros(shape=(10, 10, 2))
+        visits = np.zeros(shape=(10, 10, 2))
+        for _ in tqdm(range(eps)):
+            state_sequence = []
+            # Deal dealer
+            dealer_showing = VALUES[np.random.randint(13)]
+            if dealer_showing == 1:
+                dealer_hand = 11
+                dealer_ace = 1
+            else:
+                dealer_hand = dealer_showing
+                dealer_ace = 0
+            dealer_hand, dealer_ace = deal_card(dealer_hand, dealer_ace)
+            # Deal player
+            player_hand = 0
+            player_ace = 0
+            player_hand, player_ace = deal_card(player_hand, player_ace)
+            player_hand, player_ace = deal_card(player_hand, player_ace)
+            # Go to at least 12
+            while player_hand < 12:
+                player_hand, player_ace = deal_card(player_hand, player_ace)
+
+            # Execute policy that sticks on only 20 and 21
+            while player_hand < 20:
+                state_sequence.append((player_hand - 12, player_ace))
+                player_hand, player_ace = deal_card(player_hand, player_ace)
+
+            if player_hand > 21:
+                reward = -1
+            else:
+                state_sequence.append((player_hand - 12, player_ace))
+
+                # Dealer's round to stick on only 17 or higher
+                while dealer_hand < 17:
+                    dealer_hand, dealer_ace = deal_card(
+                        dealer_hand, dealer_ace)
+
+                if dealer_hand > 21:
+                    reward = 1
+                else:
+                    if dealer_hand > player_hand:
+                        reward = -1
+                    elif dealer_hand == player_hand:
+                        reward = 0
+                    else:
+                        reward = 1
+
+            for index in range(len(state_sequence) - 1, -1, -1):
+                if state_sequence[index] not in state_sequence[:index]:
+                    index_tuple = (
+                        state_sequence[index][0], dealer_showing - 1, state_sequence[index][1])
+                    visits[index_tuple] += 1
+                    old_value = values[index_tuple]
+                    new_value = old_value + \
+                        (reward - old_value) / \
+                        visits[index_tuple]
+                    values[index_tuple] = new_value
+
+        ax = fig.add_subplot(2, 2, fig_count, projection='3d')
+        fig_count += 1
+        x = np.linspace(1, 10, 10)
+        y = np.linspace(12, 21, 10)
+        X, Y = np.meshgrid(x, y)
+        ax.plot_wireframe(X, Y, values[:, :, 1])
+        ax.set_xlabel("Dealer showing")
+        ax.set_ylabel("Player sum")
+        ax.set_title(f"usable ace, {eps} episodes")
+        set_axes_equal(ax)
+
+        ax = fig.add_subplot(2, 2, fig_count, projection='3d')
+        fig_count += 1
+        x = np.linspace(1, 10, 10)
+        y = np.linspace(12, 21, 10)
+        X, Y = np.meshgrid(x, y)
+        ax.plot_wireframe(X, Y, values[:, :, 0])
+        ax.set_xlabel("Dealer showing")
+        ax.set_ylabel("Player sum")
+        ax.set_title(f"no usable ace, {eps} episodes")
+        set_axes_equal(ax)
+
+    plt.savefig("./my_images/figure_5_1.png")
+
+
+def main():
+    figure_5_1()
+
+
+if __name__ == "__main__":
+    main()
--- chapter05/blackjack.jl
+++ chapter05/blackjack.jl
@@ -2,6 +2,7 @@ using PyPlot
 using Seaborn
 
 const CARDS = 1:13
+const ACTIONS = 1:2 # 1 for hit and 2 for stick
 const VALUES = vcat(1:10, [10, 10, 10])
 const EPS = [10000, 500000]
 
@@ -11,7 +12,7 @@ function deal_card(hand::Int, usable_ace::Int)
         hand += 11
         usable_ace = 2
     else
-        hand += card
+        hand += VALUES[card]
     end
     if hand > 21 && usable_ace == 2
         hand -= 10
@@ -102,11 +103,111 @@ function figure_5_1()
         ax.set_title("no usable ace, $eps episodes")
         ax.invert_yaxis()
     end
-    savefig("test.png")
+    savefig("./julia_images/figure_5_1.png")
+end
+
+function figure_5_2()
+    PyPlot.rc("font", size=6)
+    fig = figure()
+    subplots_adjust(wspace=0.4, hspace=0.4)
+
+    values::Array{Float64} = zeros(10, 10, 2, 2) # Dealer, player, usable ace, action
+    visits::Array{Int} = zeros(10, 10, 2, 2)
+    policy::Array{Int} = ones(10, 10, 2) # 1 for hit and 2 for stick
+    while true
+        state_action_sequence::Array{Array{Int}} = []
+        player_hand::Int = 0
+        dealer_showing::Int = VALUES[rand(CARDS)]
+        if dealer_showing == 1
+            dealer_hand::Int = 11
+            dealer_ace::Int = 2
+        else
+            dealer_hand = dealer_showing
+            dealer_ace = 1
+        end
+        dealer_hand, dealer_ace = deal_card(dealer_hand, dealer_ace)
+        player_ace::Int = 1
+        # Initial hand
+        player_hand, player_ace = deal_card(player_hand, player_ace)
+        player_hand, player_ace = deal_card(player_hand, player_ace)
+        # Get to at least 12
+        while player_hand < 12
+            player_hand, player_ace = deal_card(player_hand, player_ace)
+        end
+        # ES, first policy random
+        action = rand(ACTIONS)
+        while action == 1
+            push!(state_action_sequence, [player_hand, player_ace, action])
+            player_hand, player_ace = deal_card(player_hand, player_ace)
+            if player_hand < 21
+                action = policy[dealer_showing, player_hand - 11, player_ace]
+            else
+                action = 2
+            end
+        end
+        if player_hand > 21
+            reward::Int = -1
+        else
+            push!(state_action_sequence, [player_hand, player_ace, action])
+            # Deal dealer cards
+            while dealer_hand < 17
+                dealer_hand, dealer_ace = deal_card(dealer_hand, dealer_ace)
+            end
+            if dealer_hand > 21
+                reward = 1
+            else
+                if dealer_hand > player_hand
+                    reward = -1
+                elseif dealer_hand == player_hand
+                    reward = 0
+                else
+                    reward = 1
+                end
+            end
+        end
+        policy_stable::Bool = true
+        for i in length(state_action_sequence):-1:1
+            if in(state_action_sequence[i], state_action_sequence[1:i - 1])
+                continue
+            end
+            # Policy evaluation
+            state::Int = state_action_sequence[i][1] - 11
+            usable_ace::Int = state_action_sequence[i][2]
+            action::Int = state_action_sequence[i][3]
+            visits[state, dealer_showing, usable_ace, action] += 1
+            old_value = values[state, dealer_showing, usable_ace, action]
+            values[state, dealer_showing, usable_ace, action] =
+                old_value + (reward - old_value) / visits[state, dealer_showing, usable_ace, action]
+            # Policy improvement
+            new_policy = findmax(values[state, dealer_showing, usable_ace, :])[2]
+            if policy_stable && new_policy != policy[state, dealer_showing, usable_ace]
+                policy_stable = false
+            end
+            policy[state, dealer_showing, usable_ace] = new_policy
+        end 
+        if policy_stable
+            break
+        end
+    end
+
+    ax = fig.add_subplot(2, 2, 1)
+    heatmap(policy[:,:,1], cmap="YlGnBu", ax=ax, xticklabels=1:10, yticklabels=12:21)
+    ax.set_xlabel("Dealer showing")
+    ax.set_ylabel("Player sum")
+    ax.set_title("usable ace policy")
+    ax.invert_yaxis()
+
+    ax = fig.add_subplot(2, 2, 2)
+    heatmap(policy[:,:,2], cmap="YlGnBu", ax=ax, xticklabels=1:10, yticklabels=12:21)
+    ax.set_xlabel("Dealer showing")
+    ax.set_ylabel("Player sum")
+    ax.set_title("no usable ace policy")
+    ax.invert_yaxis()
+    savefig("./julia_images/figure_5_2.png")
 end
 
 function main()
-    figure_5_1()
+    figure_5_2()
 end
 
 main()
